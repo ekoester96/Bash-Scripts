8900a7acc55749d738a269a54c030560b0f79ea3
ekoester96 <ethan.schmidt1996@gmail.com>
2025-04-24 11:02:09 -0500

    Update archinstall.sh

--- archinstall.sh
+++ archinstall.sh
@@ -13,26 +13,33 @@
 #6: Run command "cd Bash-Scripts"
 #7: Run command "chmod 744 archinstall.sh"
 #8: Run command "./archinstall.sh"
+# When installing use default (1) and y 
 pacman -Syu
-pacman -Sy plasma-desktop
-systemctl enable sddm.service
-systemctl start sddm.service
-pacman -Sy neofetch htop
-yay -Sy brave
-pacman -Sy vlc
-pacman -Sy vscode
-pacman -Sy libreoffice
-pacman -Sy ttf-dejavu ttf-liberation noto-fonts
-yay -Sy timeshift
-pacman -Sy bluez bluez-utils
+pacman -S plasma-desktop
+pacman -S sddm
+pacman -S neofetch htop
+yay -S brave
+pacman -S vlc
+pacman -S vscode
+pacman -S libreoffice
+pacman -S ttf-dejavu ttf-liberation noto-fonts
+yay -S timeshift
+pacman -S bluez bluez-utils
 systemctl enable bluetooth
 systemctl start bluetooth
-pacman -Sy zip unzip p7zip
-pacman -Sy yakuake
-pacman -Sy ufw
-ufw allow ssh/tcp
+pacman -S zip unzip p7zip
+pacman -S yakuake
+pacman -S nano
+pacman -S iptables-nft
+pacman -S ufw
 ufw enable
+ufw reload
+ufw allow ssh/tcp
 ufw allow 443/tcp
 ufw logging medium
+ufw status
+systemctl enable sddm.service
+systemctl start sddm.service
+#switch to plasma x11 when logging in
 
 
