0d7b223ea1b8538d1f6ebfbf329e27449a3c8f7f
ekoester96 <ethan.schmidt1996@gmail.com>
2025-04-25 09:46:45 -0500

    Update archive.sh

--- archive.sh
+++ archive.sh
@@ -8,7 +8,7 @@
 # Creates an archive of the Documents folder in a directory for backups
 
 # Usage
-# backup_script (if this script is saved in a folder located in the systems path variable) otherwise use: ./backup_script
+# backup_script (if this script is saved in a folder located in the systems $PATH variable) otherwise use: ./backup_script
 
 tar -czvf /home/$USER/Backupstg/documents_backup_"$(date +%d-%m-%y_%H-%M-%S)".tar.gz --absolute-names /home/$USER/Documents
 exit 0
